bc29a9574ea9f669ad5daff91353ad693289c5f8
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-09-21 08:23:11 +0500

    revision of ch 1 onto 9

---
+++ app.js
@@ -0,0 +1,183 @@
+/* chapter no 1
+ "Alert" 
+alert ik popup show karta ha jis ma hm koi msg display karwata ha
+alert is a keyword
+there are 2 type of alert display
+no 1
+*/
+// alert("hellow world");
+// no 2
+// window.alert("hello I am madiha");
+// ______________________x____________________
+// chapter no 2
+// Variables for Strings
+//  variable name and assigns the value "Mark"
+// var name = "Mark";
+// console.log(name);    //mark 
+// -------------------------------------
+// var name = "Mark";
+// name = "Ace";
+// console.log(name);   //ace
+// ------------------------------------
+// var nationality;
+// nationality = "U.S.";
+// console.log(nationality);  //u.s
+// --------------------------------------
+// var thanx = "Thanks for your input!"
+// alert(thanx);
+// -----------------------
+// chapter no 3
+// Variables for Numbers
+// var weight = 150;
+// weight + 25;
+// console.log(weight);  //150  
+// jab hm 25 ko kisi bhi varr ma store karay ga tab + ho ga
+// ------------------------------
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+// console.log(newNum);   //30 
+// ---------------------------------
+// var originalNum = 90;
+// originalNum = originalNum + 10;
+// console.log(originalNum);  //100
+// --------------------------------------
+// var originalNum = "23";
+// var newNum = originalNum + 7;
+// console.log(newNum); // concadinate 237
+// -------------------------------------------
+// var math = 9;
+// var sub = 8;
+// var result = math - sub;
+// console.log(result);   //1
+// --------------------------------------------
+// + , - , * , %;
+// --------------------------------------------
+// ch n0 4 
+// Variable Names Legal and Illegal
+// myName ,hello_, hello$  , hello123 are legal 
+//-------------------------------------------
+// 5
+// Math expressions:
+// Familiar operators
+// --------------------------------------
+// var popularNumber = 2 + 2;
+// alert(2 + 2);    //4
+// --------------------------------------
+// var popularNumber = 12 - 24;
+// console.log(popularNumber); //-12
+// --------------------------------------
+// 6
+// Math expressions:
+// Unfamiliar operators
+//---------------------------------
+// var num = 14;
+// num++;
+// console.log(num);  //15
+//-----------------------------------
+// var num = 14;
+// num--;
+// console.log(num); //13
+//-----------------------------------------
+// 7
+// Math expressions:
+// Eliminating ambiguity
+//---------------------------------
+// var totalCost = 1 + 3 * 4;
+// console.log(totalCost);  //13  //js right to left chalti ha
+//------------------------------------------
+// var totalCost = 1 + (3 * 4);
+// console.log(totalCost);
+//-----------------------------------------------
+// var originalNum = "23";
+// var newNum = originalNum + 7;
+// console.log(newNum);
+// -------------------------------------------------
+// 5
+// Math expressions:
+// Familiar operators
+// var a = 8;
+// var b = 2;
+// console.log(a+b);  //
+// var a = 8;
+// var b = 2;
+// console.log(a-b);
+// var a = 8;
+// var b = 2;
+// console.log(a*b);
+// var a = 8;
+// var b = 2;
+// console.log(a/b);  //divide
+// var a = 8;
+// var b = 2;
+// console.log(a**b);
+// var a = "8";
+// var b = 2;
+// console.log(a+b);
+// var num = 1;
+// var newNum = ++num;
+// console.log(newNum);
+// var num = 1;
+// var newNum = --num;
+// console.log(newNum);
+// var totalCost = 1 + 3 * 4;
+// console.log(totalCost);
+// var a = (1+5*4);
+// console.log(a);
+// var totalCost = 1 + (3 * 4);
+// console.log(totalCost);
+// var totalCost = (1 + 3) * 4;
+// console.log(totalCost);
+// -------------------------------================================
+// var totalCost = 1 + 3 * 4;
+// console.log(totalCost);  //13
+//''''''''''''''''''''''''''''''
+// var totalCost = 1 + (3 * 4);
+// console.log(totalCost);  //13
+//''''''''''''''''''''''''''''''
+// var totalCost = (1 + 3) * 4;
+// console.log(totalCost);
+// BODMAS:
+// B = Brackets (پہلے بریکٹ کو حل کیا جاتا ہے)
+// O = Orders (powers یا roots کو حل کیا جاتا ہے)
+// D = Division (تقسیم)
+// M = Multiplication (ضرب)
+// A = Addition (جمع)
+// S = Subtraction (تفریق)
+// var num =  (2+3*(5-2))
+// console.log(num); //11
+// var num = (5+(5*4));
+// console.log(num);
+// var resultOfComputation = (2 * 4) * 4 + 2;  //8*4+2   //32+2 //34
+// console.log(resultOfComputation);
+// var resultOfComputation = (2 * 4) * (4 + 2);
+// console.log(resultOfComputation);   //48
+// console.log(6*8);
+// 8
+// Concatenating text strings
+// var message = "Thanks, ";
+// var userName = "Susan";
+// var banger = "!";
+// var customMess = message + userName + banger;
+// alert(customMess);
+// Thanks, Susan!
+// var a  = "thanks,"
+// var b = "Susan!";
+// alert(a+" "+b);
+// alert("2" - 1)
+// notes 
+//  plus ma dono side num ho agr ik num ik stri ho ge to concade kar da ga 
+// agr ik side str or ik side num ho bech m - ka sign ho to - karay ga
+// + ma dono side num ho jab hi + karay ga otherwise concadae kar da ga  
+// alert("2 plus 2 equals " + 2 + 2);
+// console.log("2" + 2 + 2);
+// 9
+// Prompts
+// var spec = prompt("Your species?", "human");
+// var name = prompt("enter your name ?", "Madiha");
+// var a = +prompt("enter your number");
+// var b = +prompt("enter 2 number");
+// console.log(a+ b);
+// var question = "Your species?";
+// var defaultAnswer = "human";
+// var spec = prompt(question, defaultAnswer);
+
